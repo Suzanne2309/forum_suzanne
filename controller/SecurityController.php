c08f8551b793101a820daeebb7825c9209ab12ba
Suzanne Pons <suzanne.pons68@gmail.com>
2025-11-18 09:45:34 +0100

    Ajout de pregmatch dans la fonction register pour sécuriser la création de mot de passe

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -31,30 +31,27 @@ class SecurityController extends AbstractController{
                     if($user) {
                         header("index.php?ctrl=security&action=login"); exit;
                     } else { //Sinon on va insérer l'utilisateur dans la BDD
-                        if($pass1 == $pass2 && strlen($pass1) >= 12) {
+                        //On vérifie que le mot de passe soit bien securisé (définir un pattern et utilisation de preg_match) et que les deux champs de mot de passe soit identique
+                        $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{12,}$/';
+
+                        if($pass1 == $pass2 && preg_match($pattern, $pass1, $pass2)) {
 
                             $creationDateNow  = date('Y-m-d');
                             //On définit $data pour utiliser ensuite la fonction add($data) crée dans Manager.php
                             $data = ["pseudonym" => $pseudonym, "password" => password_hash($pass1, PASSWORD_DEFAULT), "creationDate" => $creationDateNow,  "email" => $email, "aboutMe" => ""];
-                        //utiliser add($data) du Manager.php au lieu de :
-                            // $insertUser = $pdo->prepare("INSERT INTO user (pseudonym, password, creationDate, email) VALUES (:pseudo, :password, CURTIME(), :email)");
-                            // $insertUser->execute([
-                            //     "pseudonym" => $pseudonym,
-                            //     "password" => password_hash($pass1, PASSWORD_DEFAULT),
-                            //     "email" => $email
-                            // ]);
+                            //utiliser add($data) du Manager.php au lieu d'une rêquette sql
                             //Ainsi pas besoin de créer une fonction pour la rêquette dasn UserManager comme prévu au début, car add existe déjà
                             $user = $userManager->add($data);
 
-                            header("index.php?ctrl=security&action=home"); exit;
+                            header("index.php?ctrl=security&action=login"); exit;
                         } else {
-                            echo "Les mots de passe sont pas identique ou mot de passe trop court";
-                            //message "Les mots de passe sont pas identiques ou mot de passe trop court !"
+                            echo "<p class='message'>Les mots de passe sont pas identique ou mot de passe correspondant pas aux critères !</p>";
+                            //message "Les mots de passe sont pas identiques ou mot de passe correspondant pas aux critères !"
                         }
  
                     }
                 } else {
-                    echo "Problème de saisie dans les champs de formulaire";
+                    echo "<p class='message'>Problème de saisie dans les champs de formulaire</p>";
                     // message "Problème de saisie dans les champs de formulaire"
                 };
         };
--- view/security/registration.php
+++ view/security/registration.php
@@ -1,7 +1,4 @@
-<?php
 
-
-?>
 
 <div class="wrapper">
     
@@ -9,14 +6,21 @@
 
     <div class="registerForm">
         <form action="index.php?ctrl=security&action=register" method="post">
-            <label for="pseudonym">Votre pseudonyme :</label>
+            <label for="pseudonym">Votre pseudo :</label>
             <input type="text" name="pseudonym" id="pseudonym"><br>
 
             <label for="email">Votre e-amil :</label>
             <input type="email" name="email" id="email"><br>
     
-            <label for="pass1">Votre mot de passe (au moins 8 charactères) :</label>
+            <label for="pass1">Votre mot de passe :</label>
             <input type="password" name="pass1" id="pass1"><br>
+            <p>Le mot de passe doit être composé de : </p>
+            <ul>
+                <li>12 charactères au minimum</li>
+                <li>Au moins une lettre en majuscule</li>
+                <li>Au moins une lettre en miniscule</li>
+                <li>Au moins un charactère spécial</li>
+            </ul>
 
             <label for="pass2">Confirmation du mot de passe</label>
             <input type="password" name="pass2" id="pass2"><br>
