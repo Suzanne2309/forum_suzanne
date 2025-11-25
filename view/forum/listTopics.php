7a0751f5ba3a15578c069384b4863019845a9760
Suzanne Pons <suzanne.pons68@gmail.com>
2025-11-25 16:46:04 +0100

    Ajout css listTopics

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -11,18 +11,26 @@
     <?php };?>
     <div class="listTopics">
 <?php
-    foreach($topics as $topic){ ?>
-        <p><a href="index.php?ctrl=forum&action=topicDetail&id=<?= $topic->getId() ?>"><?= $topic ?></a> publié le <?= $topic->getPublicationDate() ?>, par <a href="index.php?ctrl=security&action=profil&id=<?= $topic->getUser()->getId() ?>"><?= $topic->getUser() ?></a></p>
-        <p>Catégorie : <a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $topic->getCategory()->getId() ?>"><?= $topic->getCategory() ?></a></p>
-        <form action="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId()?>" method="post">
-            <input type="submit" name="submit" value="X">
-        </form>
-    </div>
-<?php }; 
+    if (!empty($topics)) {
+        foreach($topics as $topic){ ?>
+            <div class="topicCard">
+                <h2><a href="index.php?ctrl=forum&action=topicDetail&id=<?= $topic->getId() ?>"><?= $topic ?></a></h2>
+                <p><a href="index.php?ctrl=security&action=profil&id=<?= $topic->getUser()->getId() ?>"><?= $topic->getUser() ?></a>, <?= $topic->getPublicationDate() ?></p>
+                <p>Catégorie : <a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $topic->getCategory()->getId() ?>"><?= $topic->getCategory() ?></a></p>
+                <form action="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId()?>" method="post">
+                    <input class="delete" type="submit" name="submit" value="X">
+                </form>
+            </div>
+<?php   };
+    } else {
+        echo "<p>Il y a pas encore de Post ! Soyez le premier !</p>";
+    }
+
     if (!empty($_GET["id"])) { ?>
-    <div class="addTopicBtn">
+        <div class="addTopicBtn">
             <a href="index.php?ctrl=forum&action=addTopic&id=<?= $category->getId()?>">Ajouter un post</a>
-    </div>
+        </div>
     <?php }?>
+    </div>
 
 
